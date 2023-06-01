ead9055f94afa4c439f8505e2ecb95eb9bb9cce3
chenping3 <chenping3@finchina.com>
2023-06-01 17:19:16 +0800

    chore: 修复

--- src/access.ts
+++ src/access.ts
@@ -2,9 +2,8 @@ export default (initialState: InitialState) => {
   // 在这里按照初始化数据定义项目中的权限，统一管理
   // 参考文档 https://umijs.org/docs/max/access
   console.log('initialState:', initialState);
-  const {
-    userInfo: { authority },
-  } = initialState || { userInfo: {} };
+  const { userInfo } = initialState;
+  const { authority } = userInfo || {};
   return {
     isTourist: authority === '游客',
     canEdit: authority !== '游客',
